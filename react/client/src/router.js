873a1cff9f254a05f49cfdc435e52c9c9f9b7516
John Nenniger <JPNENNIGER@GMAIL.com>
2017-03-18 11:54:28 -0700

    small changes to router

--- react/client/src/router.js
+++ react/client/src/router.js
@@ -3,21 +3,22 @@ import Home from './home'
 export default class Router extends React.Component {
   constructor (props) {
     super (props);
-        this.state = { user_logged_in: false,
-                       path: '/',
-                       user_id: 1}
-      }
+    this.state = { path: '/',
+                   email: "",
+                   token: "" }
+  }
 
 // find user is going to find the current user by their session and add it to the state
   // function FindUser(){
   //
   //     }
 
-      render () {
-        // this is where we'll have all the switch statements to render
-        // the page we want, based on the state
-        if (true){
-          return  <Home />
-        }
-      }
+  render () {
+    // this is where we'll have all the switch statements to render
+    // the page we want, based on the state
+    if (this.state.path==='/'){
+      return  <Home />
     }
+
+  }
+}
